64d028aa63977ae500c1a5433b9584269e03cba7
Iker Izaguirre Ayerregaray <iker_izagirre@hotmail.com>
2019-10-22 21:58:08 +0200

    install mdb

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
@@ -6,8 +6,11 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 
+// https://mdbootstrap.com
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule, HomeRoutingModule]
+  imports: [CommonModule, SharedModule, HomeRoutingModule, MDBBootstrapModule.forRoot()],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule {}
